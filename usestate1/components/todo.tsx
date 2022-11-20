0ac18c758d250aea9ea80b0793783fee1b047169
Razzi65 <rizmoh65@gmail.com>
2022-11-21 03:14:26 +0500

    new to-do

--- usestate1/components/todo.tsx
+++ usestate1/components/todo.tsx
@@ -2,39 +2,25 @@ import { useState } from "react"
 
 const Todo = () => {
 
-    const [item,setitem] = useState()
-
-    const [additem, setadditem] = useState([{input:"first task"}])
+    const [uservalue, setuservalue] = useState()
 
 
+    const [addvalue,setaddvalue] = useState([{userinput:"first input"}])
 
-    const addNewItem = {
-        input:item
-    }
-
-    const getitem = (e) => {
-        setitem(e.target.value)
+    const newaddvalue = {
+        userinput:uservalue
     }
 
 
-
-    const additemFn = () => {
-      
-        
-        setadditem([...additem, {input:item}])
-      
+   const onchangeHandler=(e)=> {
+        setuservalue(e.target.value)
     }
 
-
-    console.log(additem);
-
-   
-    const delItem = () => {
-        const a=additem.pop()
-        console.log(a);
-        
-
+    const onclickhandler = () => {
+            setaddvalue([...addvalue, newaddvalue])
     }
+    console.log({addvalue});
+    
     
     return (
 
@@ -44,31 +30,28 @@ const Todo = () => {
             <br />
             <div className="container w-50">
 
+            <h1> To Do App</h1>
+
+            <input type="text" onChange={(e)=>onchangeHandler(e) } /> 
+            <button onClick={()=> onclickhandler()}> click to add</button> <br/>
+            {addvalue.map((i)=>{
+                return (
+                    <div>
+                       <li> {i.userinput} <button>Delete</button></li>
+                        </div>
+                )
+            })}
 
 
-                <h1> To do App</h1>
-                <br />
 
-                <input onChange={(e)=>getitem(e)} className="form-control" type="text" placeholder="Enter Task" />
-
-                <br /><br />
-                
-                {additem.map((i)=>{
-                    return (
-                        <div>
-                           <li> {i.input} <button type="button" className="btn btn-link text-danger" onClick={()=>delItem()}>Delete </button> </li>
-                            </div>
-                    )
-                })}
-                 
+           
                 
                 <br/>
-                <button onClick={()=>additemFn()} type="button" className="btn btn-success">Click to add task</button>
+             
             <br/>
             <br/>
            
-            </div>
-        </div>
+            </div> </div>
 
 
 
