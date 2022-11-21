2be9219190cb392ff460bc78c91a5983ea62cb26
Razzi65 <rizmoh65@gmail.com>
2022-11-22 02:06:41 +0500

    new todo

--- usestate1/components/todo.tsx
+++ usestate1/components/todo.tsx
@@ -2,60 +2,13 @@ import { useState } from "react"
 
 const Todo = () => {
 
-    const [uservalue, setuservalue] = useState()
-
-
-    const [addvalue,setaddvalue] = useState([{userinput:"first input"}])
-
-    const newaddvalue = {
-        userinput:uservalue
-    }
-
-
-   const onchangeHandler=(e)=> {
-        setuservalue(e.target.value)
-    }
-
-    const onclickhandler = () => {
-            setaddvalue([...addvalue, newaddvalue])
-    }
-    console.log({addvalue});
-    
-    
     return (
-
-
-
-        <div className="container-fluid bg-info h-100 w-75 text-center">
-            <br />
-            <div className="container w-50">
-
-            <h1> To Do App</h1>
-
-            <input type="text" onChange={(e)=>onchangeHandler(e) } /> 
-            <button onClick={()=> onclickhandler()}> click to add</button> <br/>
-            {addvalue.map((i)=>{
-                return (
-                    <div>
-                       <li> {i.userinput} <button>Delete</button></li>
-                        </div>
-                )
-            })}
-
-
-
-           
-                
-                <br/>
-             
-            <br/>
-            <br/>
-           
-            </div> </div>
-
-
-
+        <div>
+            
+        </div>
     )
+
+    
 }
 
 
