32ce70f05fd509d755928535b7acb6d7da7dcb53
Razzi65 <rizmoh65@gmail.com>
2022-11-22 23:47:10 +0500

    new todo

--- usestate1/components/todo.tsx
+++ usestate1/components/todo.tsx
@@ -2,14 +2,59 @@ import { useState } from "react"
 
 const Todo = () => {
 
+    const [todoitems, settodoitems] = useState()
+
+    const [newtodo, setnewtodo] = useState([{item:"first"}])
+    
+
+    const additemFnc = () => {
+        setnewtodo([...newtodo, addeditem ])
+    }
+
+
+    const onchangehandler = (e) => {
+        settodoitems(e.target.value)
+    }
+    const addeditem = {item:todoitems}
+    
+    console.log(newtodo);
+
+
+    const onclickdelbutton = (items) =>{
+        console.log(items);
+        
+    }
+    
+
+
+
     return (
         <div>
-            
-        </div>
-    )
 
-    
+
+
+
+            <div className="container-fluid bg-info h-100 w-75 text-center">
+                <br />
+                <div className="container w-50">
+
+                    <h1> To-do App</h1>
+
+                    
+
+                    <input type={"text"}  onChange={(e)=>onchangehandler(e)} /> <button onClick={()=>additemFnc()} >Click to add</button>
+                    <br/>
+                    {newtodo.map((items)=>{
+                        return (
+                            <div>
+                                {items.item} <button onClick={()=>onclickdelbutton (items)}> Del</button>
+                                </div>
+                        )
+                    })}
+                </div> </div> </div>
+            )
+
 }
 
 
-export default Todo
+            export default Todo
