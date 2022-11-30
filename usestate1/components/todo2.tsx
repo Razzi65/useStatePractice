efd7095aa2757fd5862701e7e069dcec46882d22
Razzi65 <rizmoh65@gmail.com>
2022-12-01 03:13:16 +0500

    To do # 5 completed

--- usestate1/components/todo2.tsx
+++ usestate1/components/todo2.tsx
@@ -1,93 +1,93 @@
 import { Button } from "@chakra-ui/react"
-// import { useState } from "react"
+import { useState } from "react"
 
-// const Todo2 = () => {
+const Todo2 = () => {
 
-//     type todotype = {
-//         todoitems: string
-//     }
+    type todotype = {
+        todoitems: string
+    }
 
-//     const [firstTodo, setFirstTodo] = useState()
-//     const onchangeHandler = (e) => {
-//         setFirstTodo(e.target.value)
-//         console.log(e);
+    const [firstTodo, setFirstTodo] = useState()
+    const onchangeHandler = (e) => {
+        setFirstTodo(e.target.value)
+        console.log(e);
 
-//     }
+    }
 
-//     const todonew: todotype = {
-//         todoitems: firstTodo
-//     }
+    const todonew: todotype = {
+        todoitems: firstTodo
+    }
 
-//     const [todoitem1, settodoitem1] = useState<todotype[]>([])
+    const [todoitem1, settodoitem1] = useState<todotype[]>([])
 
-//     const onclickhandler = () => {
-//         settodoitem1([...todoitem1, todonew])
+    const onclickhandler = () => {
+        settodoitem1([...todoitem1, todonew])
 
-//     }
+    }
 
-//     const ondeletehandler = (index) => {
-//         const newList = todoitem1.filter((item, i) => i !== index)
-//         settodoitem1(newList)
-//     }
-//     const [isUpdate, setIsUpdate] = useState(false)
-//     const [docRef, setDocRef] = useState()
+    const ondeletehandler = (index) => {
+        const newList = todoitem1.filter((item, i) => i !== index)
+        settodoitem1(newList)
+    }
+    const [isUpdate, setIsUpdate] = useState(false)
+    const [docRef, setDocRef] = useState()
 
-//     const onupdateHandler = (index, value) => {
-//         setIsUpdate(true)
-//         setFirstTodo(value)
-//         setDocRef(index)
-
-
-//     }
-
-//     const handleUpdate = () => {
-//         const newList = todoitem1.map((item, i) => {
-//             if (i === docRef) {
-//                 let updateItem = {
-//                     todoitems: firstTodo
-//                 }
-//                 return updateItem
-//             } else {
-//                 return item
-//             }
-//         })
-//         settodoitem1(newList)
-//         setFirstTodo('')
-//         setIsUpdate(false)
-//     }
+    const onupdateHandler = (index, value) => {
+        setIsUpdate(true)
+        setFirstTodo(value)
+        setDocRef(index)
 
 
+    }
+
+    const handleUpdate = () => {
+        const newList = todoitem1.map((item, i) => {
+            if (i === docRef) {
+                let updateItem = {
+                    todoitems: firstTodo
+                }
+                return updateItem
+            } else {
+                return item
+            }
+        })
+        settodoitem1(newList)
+        setFirstTodo('')
+        setIsUpdate(false)
+    }
 
 
-//     return (
 
-//         <div className="container-fluid bg-info h-100 w-75 text-center">
-//             <br />
-//             <div className="container w-50">
 
-//                 <h1> To-do App</h1>
+    return (
 
-//                 <input type="text" onChange={(e) => onchangeHandler(e)} value={firstTodo} placeholder={"Write something"} />
+        <div className="container-fluid bg-info h-100 w-75 text-center">
+            <br />
+            <div className="container w-50">
 
-//                 <br />
-//                 {todoitem1.map((item, index) => {
-//                     return (
-//                         <div>
-//                             <li> {item.todoitems} <button onClick={() => ondeletehandler(index)}>Del</button>
-//                                 <button onClick={() => onupdateHandler(index, item.todoitems)}>Edit</button></li>
+                <h1> To-do App</h1>
 
-//                         </div>
-//                     )
-//                 })}
-//                 <br />
-//                 {
-//                     isUpdate ? <button onClick={() => handleUpdate()}> Click to UPdate</button> :
-//                         <button onClick={() => onclickhandler()}> Click to Add</button>
-//                 }
-//             </div>
-//         </div>
-//     )
-// }
+                <input type="text" onChange={(e) => onchangeHandler(e)} value={firstTodo} placeholder={"Write something"} />
 
-// export default Todo2
+                <br />
+                {todoitem1.map((item, index) => {
+                    return (
+                        <div>
+                            <li> {item.todoitems} <button onClick={() => ondeletehandler(index)}>Del</button>
+                                <button onClick={() => onupdateHandler(index, item.todoitems)}>Edit</button></li>
+
+                        </div>
+                    )
+                })}
+                <br />
+                {
+                    isUpdate ? <button onClick={() => handleUpdate()}> Click to UPdate</button> :
+                        <button onClick={() => onclickhandler()}> Click to Add</button>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Todo2
 
--- usestate1/components/todo5.tsx
+++ usestate1/components/todo5.tsx
@@ -4,18 +4,18 @@ import { useState } from 'react'
 
 const Todo5 = () => {
     type toDOType5 = {
-        id?:number,
-        toDo:string
+        id?: number,
+        toDo: string
     }
 
-    const [targetValue, setTargetValue] = useState("")
-    const onChangeHandler = (e:any) => {
-        setTargetValue(e.target.value)        
+    const [targetValue, setTargetValue] = useState('')
+    const onChangeHandler = (e: any) => {
+        setTargetValue(e.target.value)
     }
 
-    const addToDo:toDOType5 = {
-        id:0,
-        toDo:targetValue
+    const addToDo: toDOType5 = {
+        id: 0,
+        toDo: targetValue
     }
 
     const [toDoList, setToDoList] = useState<toDOType5[]>([])
@@ -23,13 +23,45 @@ const Todo5 = () => {
         setToDoList([...toDoList, addToDo])
     }
 
-    const onDeleteHandler = () => {
-        
+    const onDeleteHandler = (indexToDo:number) => {
+       const filteredArry = toDoList.filter((toDOArr, newIndex)=>{
+            if(indexToDo != newIndex){
+                return toDOArr
+            }
+        })  
+        setToDoList(filteredArry)      
     }
 
+    const [isUpdate, setIsUpdate] = useState(false)
+    const [indexToDel, setIndexToDel] = useState<number>()
 
+    const onupdateHandler = (value, indexToDo:number) => {
+        setIsUpdate(true)
+        setIndexToDel(indexToDo)
+        setTargetValue(value)
 
+    }
+
+    const onEditHandler = () => {
+       const newArray = toDoList.map((arrayToDo,indexofArryToDo)=>{
+            if (indexToDel == indexofArryToDo ){
+
+                let newAddToDo:toDOType5 = {
+                    toDo : arrayToDo.toDo
+                    }
+                
+                return newAddToDo
+            }
+            else { return arrayToDo}
+        })
+
+        setIsUpdate(false)
+        setToDoList(newArray)
+        setTargetValue('')
+
+    }
     
+
     return (
 
         <Flex borderRadius={"5"} marginLeft={"400"} marginTop={"100"} bg='teal' w='50%' h='500px' p={4} color='white' justifyContent={"center"}>
@@ -37,30 +69,36 @@ const Todo5 = () => {
             <div> <h1 className='mt-3 mb-3 text-center'> To-do App</h1>
                 <div className="input-group mb-3">
 
-                <input onChange={(e)=>onChangeHandler(e)} type="text" value={targetValue} className="form-control" placeholder="Write Task" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                              
-                <button onClick={()=>onClickHandler()} type="button" className="btn btn-primary">+</button> </div>
-                <br/>
-                
-                {(targetValue || targetValue.length)? "" : "No task to Display"}
-                
+                    <input onChange={(e) => onChangeHandler(e)} type="text" value={targetValue} className="form-control" placeholder="Write Task" aria-label="Recipient's username" aria-describedby="button-addon2" />
+
+                    {(isUpdate==true)? <button onClick={() => onEditHandler()} type="button" className="btn btn-primary">Update</button> :
+                    <button onClick={() => onClickHandler()} type="button" className="btn btn-primary">+</button>} </div>
+               
+               
+                <br />
+
+                {(targetValue || targetValue.length) ? "" : "No task to Display"}
+
                 <div>
-                {
-                    toDoList.map((arrToDo, indexToDo)=>{
-                        return(
-                            <div>
-                                <ul className="list-group p-1">
-                                    <li className="list-group-item list-group-item-info">{arrToDo.toDo}</li>
-                                </ul>
+                    {
+                        toDoList.map((arrToDo, indexToDo) => {
+                            return (
+                                <div>
+                                    <ul className="list-group p-1">
+                                        <li className="list-group-item list-group-item-info">{arrToDo.toDo}
+                                            <button type="button" className="btn btn-secondary btn-sm float-end ms-1" onClick={()=>onDeleteHandler(indexToDo)}>Del</button>
+                                            <button type="button" className="btn btn-secondary btn-sm float-end ms-1" onClick={()=>onupdateHandler(arrToDo.toDo,indexToDo)}>Edit</button>
+                                        </li>
+                                    </ul>
                                 </div>
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
                 </div>
 
 
-                </div>
-            
+            </div>
+
         </Flex>
     )
 }
