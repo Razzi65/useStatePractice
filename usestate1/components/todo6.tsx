34cba0c4463567853275dcaf0946a3b578c65e57
Razzi65 <rizmoh65@gmail.com>
2022-12-02 04:58:55 +0500

    To do # 6 in progress

--- usestate1/components/todo6.tsx
+++ usestate1/components/todo6.tsx
@@ -24,6 +24,22 @@ const Todo6 = () => {
         settoDoItem([...toDoItem, newTodo])
     }
 
+    const onDeleteHander = (indexItem:number) => {
+        const filteredArr = toDoItem.filter((valu, index)=> {
+            if(indexItem!=index) {
+                return (valu)
+            }
+        })
+        settoDoItem(filteredArr)
+    }
+
+    const [istrue, setIsTrue] = useState(false)
+    const [mapIndex,setMapIndex] = useState<number>()
+    const onEditHandler = (indexItem:number) => {
+        setIsTrue(true)
+        setMapIndex(indexItem)
+
+    }
  
 
 
@@ -37,16 +53,24 @@ const Todo6 = () => {
         <Flex borderRadius={"5"} marginLeft={"400"} marginTop={"100"} bg='teal' w='50%' h='500px' p={4} color='white' justifyContent={"center"}>
 
             <div> <h1 className='mt-3 mb-3 text-center'> To-do App</h1>
+            <div className="input-group mb-3">
 
-                <div>  <input onChange={(e) => onChangeHandler(e)} type="text" className="form-control" placeholder="Write Task" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                    <button onClick={() => onclickHandler()} type="button" className="btn btn-primary">+</button>  </div>
+               <input onChange={(e) => onChangeHandler(e)} type="text" className="form-control" placeholder="Write Task" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                   {(istrue==true)? <button onClick={() => onclickHandler()} type="button" className="btn btn-primary">Update</button> : 
+                    <button onClick={() => onclickHandler()} type="button" className="btn btn-primary">+</button>  }
+                    </div>
+                    <br/>
 
-                {
+              <div>  {
                     toDoItem.map((inpuvalue, indexItem) => {
                         return (
-                            <li>{inpuvalue.itemName}
-                                <button onClick={() => (onDeleteHandler(indexItem))}>Del</button>
-                            </li>
+                            <div>
+                            <ul className="list-group p-1">
+                            <li className="list-group-item list-group-item-info">{inpuvalue.itemName}
+                     <button onClick={()=>onDeleteHander(indexItem)} type="button" className="btn btn-secondary btn-sm float-end ms-1" >Del</button>
+                     <button onClick={()=>onEditHandler(indexItem)} type="button" className="btn btn-secondary btn-sm float-end ms-1" >Edit</button>
+
+                            </li> </ul> </div>
                         )
                     })
                 }
@@ -54,7 +78,7 @@ const Todo6 = () => {
 
 
 
-            </div>
+            </div></div>
         </Flex>
     )
 }
